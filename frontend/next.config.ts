c002ecc7b16113f2d04ddc9b80afbb1bc7106b2e
Vaibhav Shukla <shuklavaibhav336@gmail.com>
2025-06-02 03:58:06 +0530

    Add GitHub Actions workflows for publishing to GitHub Pages and setting up Node.js

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // For Github Pages deployment
+  output: "export",
+  reactStrictMode: true,
 };
 
 export default nextConfig;
