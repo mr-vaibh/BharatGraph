72bb79ec7c456b32aedbee3872596de0130d2ffd
Vaibhav Shukla <shuklavaibhav336@gmail.com>
2025-06-02 03:49:54 +0530

    Disable ESLint rule for explicit any types in BubbleChart and useZoom hooks

--- frontend/src/components/BubbleChart.tsx
+++ frontend/src/components/BubbleChart.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
 'use client'
 
 import * as d3 from 'd3'
--- frontend/src/hooks/useZoom.ts
+++ frontend/src/hooks/useZoom.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
 import * as d3 from 'd3'
 import { useEffect } from 'react'
 
