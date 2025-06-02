3b9780828aa0685b5ed167b074411c78c06a1085
Vaibhav Shukla <shuklavaibhav336@gmail.com>
2025-06-02 16:18:33 +0530

    Comment out output and reactStrictMode configurations in next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,8 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   // For Github Pages deployment
-  output: "export",
-  reactStrictMode: true,
+  // output: "export",
+  // reactStrictMode: true,
 };
 
 export default nextConfig;
